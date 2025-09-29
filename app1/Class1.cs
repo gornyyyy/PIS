dd18efb6e74614739a093df5f5ba079fdccf7002
gornyyyy <gornoostaiy@gmail.com>
2025-09-29 12:36:05 +0700

    Добавьте файлы проекта.

---
+++ app1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app1
+{
+    public class Pressure
+    {
+        public DateTime Date { get; set; }
+        public double Height { get; set; }
+        public int Value { get; set; }
+
+        public Pressure(DateTime date, double height, int value)
+        {
+            Date = date;
+            Height = height;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"Давление: {Value} Па (дата: {Date:yyyy.MM.dd}, высота: {Height})";
+        }
+    }
+}
---
+++ app1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app1
+{
+    internal class Program
+    {
+        public static Pressure ParsePressure(string text)
+        {
+            string[] parts = text.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length > 0)
+            {
+                DateTime date = DateTime.ParseExact(parts[0], "yyyy.MM.dd", null);
+                double height = double.Parse(parts[1]);
+                int value = int.Parse(parts[2]);
+
+                return new Pressure(date, height, value);
+            }
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            string text1 = "2023.02.28 1020,75 1";
+            Pressure pressure1 = ParsePressure(text1);
+            Console.WriteLine(pressure1);
+
+            string[] lines = File.ReadAllLines("C:\\Users\\gorno\\OneDrive\\Рабочий стол\\student\\3 семестр\\ПИС\\PIS\\app1\\Pressures.txt");
+
+            foreach (string line in lines)
+            {
+                Pressure pressure = ParsePressure(line);
+                Console.WriteLine(pressure);
+            }
+
+        }
+    }
+}
